4fd988c584fa6db173e500d802320ad655f2adff
David Cruz <cruzdavidj@gmail.com>
2018-11-06 10:24:35 -0500

    init
    init

---
+++ keyGen-master/keyGen/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  keyGen
+//
+//  Created by John Baker on 2/5/18.
+//  Copyright © 2018 B4k3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
